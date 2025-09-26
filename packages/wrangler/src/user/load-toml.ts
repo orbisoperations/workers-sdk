2267e61f6fd252e962c5e509f0aa8db146cbc689
Luis <luis.romero.sevilla.pr@gmail.com>
2025-09-26 17:51:52 -0400

    feat: better login

--- packages/wrangler/src/user/load-toml.ts
+++ packages/wrangler/src/user/load-toml.ts
@@ -1,9 +1,39 @@
+import { mkdirSync, writeFileSync } from "fs";
+import path from "path";
 import { logger } from "../logger";
 import { parseTOML, readFileSync } from "../parse";
-import { UserAuthConfig, writeAuthConfigFile } from "./user";
+import {
+	getAuthConfigFilePath,
+	reinitialiseAuthTokens,
+	UserAuthConfig,
+	writeAuthConfigFile,
+} from "./user";
 
 export const loadTOML = (file: string) => {
 	logger.log(`Loading TOML file: ${file}`);
-	const toml = parseTOML(readFileSync(file), file);
-	writeAuthConfigFile(toml as UserAuthConfig);
+	const toml = readFileSync(file);
+	logger.log(`TOML file: ${toml}`);
+	// writeAuthConfigFile(toml as UserAuthConfig);
+
+	const configPath = getAuthConfigFilePath();
+
+	mkdirSync(path.dirname(configPath), {
+		recursive: true,
+	});
+	writeFileSync(path.join(configPath), toml, {
+		encoding: "utf-8",
+	});
+
+	logger.log(`Wrote TOML file to: ${configPath}`);
+
+	reinitialiseAuthTokens();
+
+	logger.log("✅ Successfully loaded TOML file ");
+};
+
+export const showTOML = () => {
+	const configPath = getAuthConfigFilePath();
+	const toml = readFileSync(configPath);
+	logger.log("reading TOML file from: ", configPath);
+	logger.log(`TOML file: \n\n${toml}`);
 };
